c1fcb6e6a3257bf26dde0fd11502251ec5c7dd30
hitmaizer <jose.sousalves@gmail.com>
2022-01-28 18:00:53 +0000

    hamburger menu

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello mate!")
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav--ul');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('show');
+});
+
+
